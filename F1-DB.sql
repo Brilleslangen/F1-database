5b8161e2ae33094353a67473d1d83b10d3a1504c
Nicolai Sivesind <nicolts@stud.ntnu.no>
2021-11-15 19:55:45 +0100

    Add merc and mclaren

--- F1-DB.sql
+++ F1-DB.sql
@@ -95,11 +95,29 @@ ALTER TABLE car
 ADD CONSTRAINT car_fk6
 FOREIGN KEY (owner) REFERENCES team(team_id);
 
+# Mercedes
+INSERT INTO team (team_name) VALUES ("Mercedes AMG");
+INSERT INTO employee (full_name, team_id) VALUES ("Toto Wolff", 1);
+INSERT INTO employee (full_name, team_id) VALUES ("Lewis Hamilton", 1);
+INSERT INTO employee (full_name, team_id) VALUES ("Valtteri Bottas", 1);
 
+UPDATE team SET 
+	principal = 1,
+    first_driver = 2,
+    second_driver = 3
+WHERE team_id = 1;
 
+# Mclaren
+INSERT INTO team (team_name) VALUES ("Mclaren F1 Team");
+INSERT INTO employee (full_name, team_id) VALUES ("Zak Brown", 2);
+INSERT INTO employee (full_name, team_id) VALUES ("Lando Norris", 2);
+INSERT INTO employee (full_name, team_id) VALUES ("Daniel Ricciardo", 2);
 
-
-
+UPDATE team SET 
+	principal = 4,
+    first_driver = 5,
+    second_driver = 6
+WHERE team_id = 2;
 
 # Red Bull
 INSERT INTO team (team_name) VALUES ("Red Bull Racing Honda");
